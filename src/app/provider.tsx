2aa012e84d4c563efefcd70ecc02751ea534833a
kangjuhyup <fog0510@gamil.com>
2023-06-03 12:42:01 +0900

    add footer

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,7 +1,19 @@
 import Wagmi from "@/components/common/wagmi";
 
+  
+
+const rootStyles = {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+
 const RootProvider = ({ children }: { children: React.ReactNode }) => {
-    return (<div>
+    return (
+    <div style={rootStyles}>
         <Wagmi>
             {children}
         </Wagmi>
